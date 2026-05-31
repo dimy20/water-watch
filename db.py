496595f545ee227acb1a4606b4dfde255c440da1
enzo <enzostrindade16@outlook.com>
2026-05-31 16:34:15 -0300

    separacion de usurios: ETL y streamlit

--- db.py
+++ db.py
@@ -1,16 +1,20 @@
-import psycopg # Libreria de python para establecer una conexio directa con PostgreSQL.
+import psycopg
 import os
 from psycopg import Connection
 
 from pymongo import MongoClient
 from pymongo.database import Database
 
-DATABASE_URL = os.environ["DATABASE_URL"]
-MONGO_URL = os.environ["MONGO_URL"]
-
 def get_postgres_conn() -> Connection:
-    return psycopg.connect(DATABASE_URL)
+    return psycopg.connect(os.environ["ETL_DATABASE_URL"])
 
 def get_mongo_conn() -> Database:
-    client = MongoClient(MONGO_URL, uuidRepresentation="standard")
+    client = MongoClient(os.environ["ETL_MONGO_URL"], uuidRepresentation="standard")
+    return client["grp05db"]
+
+def get_streamlit_postgres_conn() -> Connection:
+    return psycopg.connect(os.environ["STREAMLIT_DATABASE_URL"])
+
+def get_streamlit_mongo_conn() -> Database:
+    client = MongoClient(os.environ["STREAMLIT_MONGO_URL"], uuidRepresentation="standard")
     return client["grp05db"]
